dcabe8e78f50644b252eb3a363b6d784780b04f2
whitecoldd <buyukliwc@gmail.com>
2023-06-01 03:07:16 +0200

    deploy fix

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIRE_API_KEY,
+  authDomain: import.meta.env.VITE_FIRE_DOMAIN,
+  projectId: import.meta.env.VITE_FIRE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRE_SENDER_ID,
+  appId: import.meta.env.VITE_FIRE_APP_ID,
+  measurementId: import.meta.env.VITE_FIRE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
