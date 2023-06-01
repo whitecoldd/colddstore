4cf0a08977980b2da0e0ca4a2c0a2fedc647f94a
whitecoldd <buyukliwc@gmail.com>
2023-06-01 15:59:02 +0200

    deploy fix n10

--- client/src/req.js
+++ client/src/req.js
@@ -7,6 +7,7 @@ import axios from "axios";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
+console.log(TOKEN)
 
 export const publicRequest = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
