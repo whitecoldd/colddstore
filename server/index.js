bb8a0cb6983f05ad8c94233aa57b7684d3ff82fb
whitecoldd <buyukliwc@gmail.com>
2023-06-01 04:00:10 +0200

    deploy fix n6

--- server/index.js
+++ server/index.js
@@ -14,7 +14,23 @@ const cors = require("cors");
 //configs and connections
 dotenv.config();
 connectDB();
-app.use(cors({ origin: ["https://colddstore.onrender.com/", "https://colddstore.netlify.app/",] }));
+app.use(
+  cors({
+    origin: [
+      "https://colddstore.onrender.com/",
+      "https://colddstore.netlify.app/",
+    ],
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://main--colddstore.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 
 //routes
